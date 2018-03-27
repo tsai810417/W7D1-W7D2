c353a4fe3e2863c088f50b68cde5ad79ee5eed68
Cheng Tsai <tsai_cheng@yahoo.com>
2018-03-27 16:58:56 -0700

    finish w7d2

--- todos/webpack.config.js
+++ todos/webpack.config.js
@@ -3,7 +3,7 @@ var path = require('path');
 module.exports = {
   entry: './frontend/todo_redux.jsx',
   output: {
-    filename: './bundle.js',
+    filename: './app/assests/javascripts/bundle.js',
   },
   module: {
     loaders: [
